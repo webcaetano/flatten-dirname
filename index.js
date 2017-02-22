579d3485ea2f1d685b7d1f21cf1ff3ba5f61ef4a
Andre Caetano <webandrecaetano@gmail.com>
2017-02-22 05:41:31 -0300

    index: inital

---
+++ index.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var fs = require('fs-extra');
+var glob = require('glob');
+var async = require('async');
+var _ = require('lodash');
+
+// insert defaults here
+var defaults = {
+	split:'_$_'
+}
+
+var self = function(src, dest, options, done){
+	if(typeof options==='function') {
+		var tmpVar = options;
+		options = done ? done : {};
+		done = tmpVar;
+	}
+	options = _.extend({},defaults,options);
+
+	async.auto({
+		files:function(callback){
+			glob(src,{nodir:true},callback);
+		},
+		rename:['files',function(results,callback){
+			var run = [];
+
+			_.each(results.files,function(file){
+				var data = path.parse(file);
+				var newName = path.format({
+					dir:dest,
+					name:path.relative(dest,file).replace(/\//g,options.split),
+					ext:'',
+				});
+
+				run.push(function(callback){
+					fs.copy(file,newName,callback);
+				});
+			});
+
+			async.parallel(run,callback);
+		}]
+	},function(err,results){
+		if(done) done(err,results);
+	})
+}
+
+module.exports = self;
